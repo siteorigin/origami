eda7527ad8a8a31e5e1ca28c2c6f0fcbf1c3adc5
gregpriday <gregpriday@965b8dba-cb3b-494d-9706-4adfdc746276>
2012-05-07 15:29:00 +0000

    Tagged Origami as 1.0

---
+++ themes/origami/tags/1.0/content-video.php
@@ -0,0 +1,15 @@
+<h1 class="entry-title">
+	<a href="<?php print get_post_permalink() ?>"><?php the_title() ?></a>
+</h1>
+
+<div class="post-info">
+	<?php printf(__('On %s', 'origami'), get_the_date()) ?>
+	<?php if(simple_options_get('display', 'post_author')) printf(__('By %s', 'origami'), '<a href="'.get_author_posts_url(get_the_author_meta('ID')).'">'.get_the_author_meta('display_name').'</a>'); ?>
+	<?php if(simple_options_get('display', 'comment_counts') && simple_options_get('display', 'post_author')) _e('With', 'origami') ?>
+	<?php if(simple_options_get('display', 'comment_counts')) printf(__('<strong>%u</strong> Comments', 'origami'), $post->comment_count); ?>
+</div>
+
+<?php $columns = get_post_meta($post->ID, 'content_columns', true); if(!$columns) $columns = 2; ?>
+<div class="content">
+	<?php the_content(); ?>
+</div>
---
+++ themes/origami/tags/1.0/footer.php
@@ -0,0 +1,23 @@
+		<?php $sidebars_widgets = wp_get_sidebars_widgets(); ?>
+		
+		<?php if(!empty($sidebars_widgets['site-footer'])) : ?>
+			<div id="footer-widgets">
+				<?php dynamic_sidebar('Footer') ?>
+				<div class="clear"></div>
+			</div>
+		<?php endif; ?>
+	</div>
+	
+	<div id="footer">
+		<?php if(simple_options_get('messages', 'copyright') != '') : ?>
+			<div class="copyright"><?php print simple_options_get('messages', 'copyright') ?></div>
+		<?php endif; ?>
+
+		<?php origami_attribution_footer('<div class="designed">', '</div>') ?>
+		<div class="clear"></div>
+	</div>
+</div>
+
+<?php wp_footer() ?>
+</body>
+</html>
