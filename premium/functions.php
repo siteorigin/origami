fc0765ebacc2a4f032a4077dc1423a8e9dfd1848
Greg Priday <greg@siteorigin.com>
2012-07-10 20:56:04 +0000

    Added start of premium

---
+++ premium/functions.php
@@ -0,0 +1,41 @@
+<?php
+
+define('SO_IS_PREMIUM', true);
+
+require_once(get_stylesheet_directory().'/functions/settings.php');
+
+// Include all the premium features
+require_once(get_stylesheet_directory().'/extras/css/css.php');
+require_once(get_stylesheet_directory().'/extras/ajax-comments/ajax-comments.php');
+require_once(get_stylesheet_directory().'/extras/responsive-widgets/responsive-widgets.php');
+require_once(get_stylesheet_directory().'/extras/share/share.php');
+
+/**
+ * Setup Origami Premium. Runs after Origami is set up.
+ * 
+ * @action after_setup_theme
+ */
+function origami_premium_setup(){
+	// Activate all the extras
+	if(so_setting('comments_ajax')) so_ajax_comments_activate();
+	if(so_setting('responsive_footer')) so_responsive_widget_activate('site-footer', '#footer-widgets .widget');
+	if(so_setting('social_share')) so_share_activate();
+}
+add_action('after_setup_theme', 'origami_premium_setup', 11);
+
+/**
+ * Enqueue Origami Premium scripts
+ */
+function origami_premium_enqueue(){
+	wp_enqueue_style('origami-premium', get_stylesheet_directory_uri().'/style.css', array(), SO_THEME_VERSION);
+}
+add_action('wp_enqueue_scripts', 'origami_premium_enqueue');
+
+/**
+ * Create the stylesheet URL
+ */
+function origami_premium_filter_stylesheet_url(){
+	return get_template_directory_uri().'/style.css';
+}
+add_filter('stylesheet_uri', 'origami_premium_filter_stylesheet_url', 10, 2);
+
