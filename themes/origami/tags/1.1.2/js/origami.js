9ba91631518d63921d88e6da515e8590db4a84f7
gregpriday <gregpriday@965b8dba-cb3b-494d-9706-4adfdc746276>
2012-07-11 23:05:53 +0000

    Tagged Origami

---
+++ themes/origami/tags/1.1.2/js/origami.js
@@ -0,0 +1,23 @@
+jQuery(function ($) {
+    // We use FitVids to scale videos to mobile devices 
+    $('.featured-video, .content').fitVids();
+
+    // FlexSlider is a great responsive slider
+    $('.flexslider').flexslider();
+
+    // Test and load polyfills
+    Modernizr.load({
+        test : Modernizr.inlinesvg,
+        nope : origami.polyfills + '/sie-mini.js'
+    });
+
+    // Init the placeholder
+    Modernizr.load({
+        test : Modernizr.input.autocomplete,
+        nope : origami.polyfills + '/jquery.placeholder.min.js',
+        complete:function () {
+            if (Modernizr.input.autocomplete) return;
+            $('input[placeholder], textarea[placeholder]').placeholder();
+        }
+    });
+});
