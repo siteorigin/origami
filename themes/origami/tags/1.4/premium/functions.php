d81e71643e0f6d15d55e98072183f064b8a4d77b
gregpriday <gregpriday@965b8dba-cb3b-494d-9706-4adfdc746276>
2013-01-30 23:32:48 +0000

    Tagged Origami

---
+++ themes/origami/tags/1.4/premium/functions.php
@@ -0,0 +1,54 @@
+<?php
+
+define('SITEORIGIN_IS_PREMIUM', true);
+
+include get_template_directory().'/premium/functions/settings.php';
+include get_template_directory().'/premium/functions/gallery.php';
+
+// Include all the premium extras
+include get_template_directory().'/premium/extras/share/share.php';
+include get_template_directory().'/premium/extras/css/css.php';
+include get_template_directory().'/premium/extras/ajax-comments/ajax-comments.php';
+include get_template_directory().'/premium/extras/responsive-widgets/responsive-widgets.php';
+include get_template_directory().'/premium/extras/widgets/widgets.php';
+
+/**
+ * Setup Origami Premium. Runs after Origami is set up.
+ * 
+ * @action after_setup_theme
+ */
+function origami_premium_setup(){
+	// Activate all the extras
+	if(siteorigin_setting('comments_ajax')) siteorigin_ajax_comments_activate();
+	if(siteorigin_setting('responsive_footer')) siteorigin_responsive_widget_activate('site-footer', '#footer-widgets .widget');
+	if(siteorigin_setting('social_share')) siteorigin_share_activate();
+}
+add_action('after_setup_theme', 'origami_premium_setup', 11);
+
+/**
+ * Enqueue Origami Premium scripts
+ */
+function origami_premium_enqueue(){
+	wp_enqueue_style('origami-premium', get_template_directory_uri().'/premium/premium.css', array(), SITEORIGIN_THEME_VERSION);
+}
+add_action('wp_enqueue_scripts', 'origami_premium_enqueue');
+
+/**
+ * Create the stylesheet URL
+ */
+function origami_premium_filter_stylesheet_url(){
+	return get_template_directory_uri().'/style.css';
+}
+add_filter('stylesheet_uri', 'origami_premium_filter_stylesheet_url', 10, 2);
+
+/**
+ * Register all widgets
+ */
+function origami_premium_widgets_init() {
+	register_widget( 'SiteOrigin_Widgets_GoogleMap' );
+	register_widget( 'SiteOrigin_Widgets_Video' );
+	//register_widget( 'SiteOrigin_Widgets_List' );
+	//register_widget( 'SiteOrigin_Widgets_CF7' );
+}
+
+add_action( 'widgets_init', 'origami_premium_widgets_init' );
