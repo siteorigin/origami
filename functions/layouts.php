bc3ef19aa9f0e76265a4842b8003a08390f8989a
Greg Priday <greg@siteorigin.com>
2013-02-01 23:19:15 +0000

    General updates for the new Panels

--- functions/layouts.php
+++ functions/layouts.php
@@ -6,7 +6,7 @@
  * @param $layouts
  */
 function origami_prebuilt_page_layouts($layouts){
-	$layouts[] = array (
+	$layouts['business_home'] = array (
 		'name' => __('Business Home Page', 'origami'),
 		'widgets' =>
 		array (
