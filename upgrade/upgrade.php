f51044fc4bf182c6c04ea06e9a8eaafbde0fbb10
Greg Priday <greg@siteorigin.com>
2013-02-05 05:29:08 +0000

    Added to readme.txt and fixed price typo.

--- upgrade/upgrade.php
+++ upgrade/upgrade.php
@@ -2,7 +2,7 @@
 
 function origami_premium_upgrade_content($content){
 	$content['premium_title'] = __('Upgrade To Origami Premium', 'origami');
-	$content['premium_summary'] = __("If you've enjoyed using Origami, you'll going to love Origami Premium. It's a robust upgrade to Origami that gives you loads of cool features and email support. At just <strong>$5</strong>, it's a cost effective way to give your site a professional edge.", 'origami');
+	$content['premium_summary'] = __("If you've enjoyed using Origami, you'll going to love Origami Premium. It's a robust upgrade to Origami that gives you loads of cool features and email support. For just a few dollars, it's a cost effective way to give your site a professional edge.", 'origami');
 
 	$content['buy_url'] = 'http://siteorigin.fetchapp.com/sell/aqueifoo';
 	$content['buy_price'] = '$9';
