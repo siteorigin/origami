880dd6688b7235aca9859b5cb83d2d8c67453c4f
gregpriday <gregpriday@965b8dba-cb3b-494d-9706-4adfdc746276>
2013-05-09 09:19:58 +0000

    Fixed notice

--- themes/origami/trunk/functions.php
+++ themes/origami/trunk/functions.php
@@ -4,17 +4,17 @@ define('SITEORIGIN_THEME_VERSION', 'trunk');
 define('SITEORIGIN_THEME_UPDATE_ID', 269);
 define('SITEORIGIN_THEME_ENDPOINT', 'http://siteorigin.localhost');
 
-include get_template_directory().'/extras/premium/premium.php';
-include get_template_directory().'/extras/settings/settings.php';
-include get_template_directory().'/extras/adminbar/adminbar.php';
-include get_template_directory().'/extras/widgets/widgets.php';
+include get_template_directory() . '/extras/premium/premium.php';
+include get_template_directory() . '/extras/settings/settings.php';
+include get_template_directory() . '/extras/adminbar/adminbar.php';
+include get_template_directory() . '/extras/widgets/widgets.php';
 include get_template_directory() . '/extras/updater/updater.php';
 include get_template_directory() . '/extras/update/update.php';
 include get_template_directory() . '/extras/plugin-activation/plugin-activation.php';
 
-include get_template_directory().'/functions/settings.php';
-include get_template_directory().'/functions/gallery.php';
-include get_template_directory().'/functions/panels.php';
+include get_template_directory() . '/functions/settings.php';
+include get_template_directory() . '/functions/gallery.php';
+include get_template_directory() . '/functions/panels.php';
 
 if( file_exists(get_template_directory().'/premium/functions.php') ) {
 	// If this is the premium version
@@ -297,7 +297,7 @@ add_filter('the_content', 'origami_content_filter', 8);
 function origami_print_styles(){
 	// Create the footer widget CSS
 	$sidebars_widgets = wp_get_sidebars_widgets();
-	$count = count($sidebars_widgets['site-footer']);
+	$count = isset($sidebars_widgets['site-footer']) ? count($sidebars_widgets['site-footer']) : 1;
 	$count = max($count,1);
 
 	?>
