e6c11f42a625ef4a0cd99bcae2e95fc31ca171b2
Greg Priday <greg@siteorigin.com>
2013-06-05 15:19:37 +0000

    Small changes to premium information.

--- header.php
+++ header.php
@@ -15,8 +15,8 @@
 <body <?php body_class() ?>>
 <div class="container">
 	<div id="page-container">
-		<div id="logo" <?php if(siteorigin_setting('display_logo_centered')) echo 'class="logo-centered"' ?>>
-			<a href="<?php echo esc_url(home_url('/')) ?>" title="<?php echo esc_attr(get_bloginfo('description')) ?>" class="logo-link">
+		<div id="logo" <?php if( siteorigin_setting( 'display_logo_centered' ) ) echo 'class="logo-centered"' ?>>
+			<a href="<?php echo esc_url( home_url( '/' ) ) ?>" title="<?php echo esc_attr( get_bloginfo( 'description' ) ) ?>" class="logo-link">
 				<?php if( get_header_image() ) : ?>
 					<img src="<?php esc_url(header_image()); ?>" height="<?php echo get_custom_header()->height; ?>" width="<?php echo get_custom_header()->width; ?>" alt="<?php echo esc_attr(get_bloginfo('name')) ?>" />
 				<?php else : ?>
