7afe19b203374aeb74c671cedf158b9ad45d76b1
Greg Priday <greg@siteorigin.com>
2013-07-30 09:57:43 +0000

    Various fixes and updates. Made Origami more child themeable.

--- footer.php
+++ footer.php
@@ -1,27 +1,29 @@
 		<?php $sidebars_widgets = wp_get_sidebars_widgets(); ?>
-		
+
+
+
 		<?php if(!empty($sidebars_widgets['site-footer'])) : ?>
-			<div id="footer-widgets">
-				<div id="footer-widgets-wrapper">
-					<?php dynamic_sidebar('Footer') ?>
-					<div class="clear"></div>
-				</div>
-			</div>
+
+			<?php do_action('origami_before_footer_widgets') ?>
+
+			<?php get_template_part('parts/footer-widgets') ?>
+
+			<?php do_action('origami_after_footer_widgets') ?>
+
 		<?php endif; ?>
 
 		<?php do_action('origami_bottom_page_container') ?>
+
 	</div>
 
 	<?php do_action('origami_after_page_container') ?>
 
-	<div id="footer">
-		<?php if(siteorigin_setting('text_copyright') != '') : ?>
-			<div class="copyright"><?php echo wp_kses_post(siteorigin_setting('text_copyright')) ?></div>
-		<?php endif; ?>
+	<?php do_action('origami_before_footer') ?>
 
-		<?php echo apply_filters('siteorigin_attribution_footer', '<div class="designed">' . sprintf(__('Theme By %s', 'origami'), '<a href="http://siteorigin.com">SiteOrigin</a>') . '</div>') ?>
-		<div class="clear"></div>
-	</div>
+	<?php get_template_part('parts/footer-copyright') ?>
+
+	<?php do_action('origami_after_footer') ?>
+
 </div>
 
 <?php wp_footer() ?>
