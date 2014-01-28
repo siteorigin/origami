4dbe7fa9dac4236f6041cd455e10edb2023297b4
Greg Priday <greg@siteorigin.com>
2014-01-28 19:03:34 +0000

    * Fixed possible issue with header image. * Search bar drops below logo on mobile.

--- parts/logo.php
+++ parts/logo.php
@@ -1,7 +1,6 @@
 <div id="logo" <?php if( siteorigin_setting( 'display_logo_centered' ) ) echo 'class="logo-centered"' ?>>
 	<a href="<?php echo esc_url( home_url( '/' ) ) ?>" title="<?php echo esc_attr( get_bloginfo( 'description' ) ) ?>" class="logo-link">
-		<?php if( get_header_image() ) : ?>
-			<img src="<?php esc_url(header_image()); ?>" height="<?php echo get_custom_header()->height; ?>" width="<?php echo get_custom_header()->width; ?>" alt="<?php echo esc_attr(get_bloginfo('name')) ?>" />
+		<?php if( get_header_image() ) : origami_header_image(); ?>
 		<?php else : ?>
 			<h1 class="logo"><?php bloginfo('name') ?></h1><br/>
 			<h3 class="logo"><?php bloginfo('description') ?></h3>
