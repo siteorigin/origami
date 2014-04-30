7c8e59c12585ec3309897bf5881204ece75bd1d5
Greg Priday <greg@siteorigin.com>
2014-04-30 11:39:03 +0000

    * Added setting to disable responsive mode. * Added IE edge header to disable Internet Explorer compatibility mode. * Improved bundled language file.

--- header.php
+++ header.php
@@ -2,7 +2,6 @@
 <html <?php language_attributes(); ?>>
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=<?php bloginfo( 'charset' ); ?>" />
-	<meta name='viewport' content='width=device-width, initial-scale=1.0, maximum-scale = 1.0, user-scalable=0' />
 
 	<title><?php wp_title('|', true, 'right'); ?></title>
 	
