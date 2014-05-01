180c4a926590ad3aea1034f21650935f1fe52f79
gregpriday <gregpriday@965b8dba-cb3b-494d-9706-4adfdc746276>
2014-05-01 12:35:49 +0000

    Tagged Origami

--- themes/origami/tags/1.5.17/index.php
+++
@@ -1,9 +0,0 @@
-<?php get_header() ?>
-
-<div><?php get_template_part('loop', 'index') ?></div>
-
-<div id="posts-nav">
-	<?php posts_nav_link('', __('Newer Entries', 'origami'), __('Older Entries', 'origami')); ?>
-</div>
-
-<?php get_footer() ?>
--- themes/origami/tags/1.5.17/parts/menu-top.php
+++
@@ -1,8 +0,0 @@
-<nav id="menu" class="primary">
-	<?php
-		wp_nav_menu(array(
-			'theme_location' => 'primary',
-			'menu_id' => 'main-menu',
-		));
-	?>
-</nav>
