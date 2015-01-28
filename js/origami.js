3e1593d9349a4adf4f5e6e6579f42be6b7b2e79c
gregpriday <greg@siteorigin.com>
2015-01-29 01:40:55 +0200

    Origami Updates.
    * Update HTML5Shiv
    * Removed Modernizr to prevent JS error.
    * Changed how minified JS files are enqueued.
    * Small CSS tweaks.

--- js/origami.js
+++ js/origami.js
@@ -12,36 +12,4 @@ jQuery(function ($) {
     // FlexSlider is a great responsive slider
     $('.flexslider').flexslider();
 
-    // Test and load polyfills
-    if(!Modernizr.inlinesvg) {
-        // No support for SVG, so replace with images where possible
-        $('svg').each(function(){
-            var $$ = $(this);
-
-            if( typeof $$.data('replacement') != 'undefined' ) {
-
-                $$.replaceWith(
-                    $('<img>')
-                        .attr( {
-                            'src' : $$.data('replacement')
-                        } )
-                        .css( {
-                            'width' : $$.attr('width'),
-                            'height' : $$.attr('height')
-                        } )
-                );
-
-            }
-        })
-    }
-
-    // Init the placeholder
-    Modernizr.load({
-        test : Modernizr.input.autocomplete,
-        nope : origami.polyfills + '/jquery.placeholder.min.js',
-        complete:function () {
-            if (Modernizr.input.autocomplete) return;
-            $('input[placeholder], textarea[placeholder]').placeholder();
-        }
-    });
 });
