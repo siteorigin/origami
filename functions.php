fa779276ce56b0fe94d00630cafd8c5b306d664e
Greg Priday <greg@siteorigin.com>
2015-07-07 11:44:19 +0200

    Updated theme version

--- functions.php
+++ functions.php
@@ -1,7 +1,7 @@
 <?php
 
-define('SITEORIGIN_THEME_VERSION', 'trunk');
-define('SITEORIGIN_THEME_ENDPOINT', 'http://updates.siteorigin.com');
+define( 'SITEORIGIN_THEME_VERSION' , 'dev' );
+define( 'SITEORIGIN_THEME_ENDPOINT' , 'http://updates.siteorigin.com' );
 
 include get_template_directory() . '/extras/premium/premium.php';
 include get_template_directory() . '/extras/settings/settings.php';
