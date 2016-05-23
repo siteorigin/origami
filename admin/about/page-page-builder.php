9234c0939f90dcf6bb7c7f9fe04bd7d8994972c9
gregpriday <greg@siteorigin.com>
2016-05-23 08:47:22 +0200

    Improved about page content.

--- admin/about/page-page-builder.php
+++ admin/about/page-page-builder.php
@@ -1,5 +1,5 @@
 <h3><?php _e( 'Enhanced by Page Builder', 'origami' ) ?></h3>
 <p>
-	<?php printf( __( "Origami is integrated with our %sfree Page Builder%s plguin.", 'origami' ), '<a href="https://siteorigin.com/page-builder/">', '</a>' ) ?>
+	<?php printf( __( "Origami integrates, beautifully, with our %sfree Page Builder%s plugin.", 'origami' ), '<a href="https://siteorigin.com/page-builder/">', '</a>' ) ?>
 	<?php _e( 'This powerful plugin gives you full drag and drop capabilities right inside Origami.', 'origami' ) ?>
 </p>
