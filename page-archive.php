a163c07e7d36a95e69d5aadf9b582da748fea544
Alex S <alex@siteorigin.com>
2016-11-23 18:05:32 +1000

    Added featured image to page archive

--- page-archive.php
+++ page-archive.php
@@ -4,6 +4,12 @@ update_post_meta( get_the_ID(), '_wp_page_template', 'template-archive.php' );
 ?>
 
 <div <?php post_class('post') ?>>
+	<?php if( has_post_thumbnail() && siteorigin_setting( 'display_featured_image' ) ) : ?>
+		<div class="featured-image">
+			<?php the_post_thumbnail( null, array( 'class' => 'main-image' ) ) ?>
+		</div>
+	<?php endif; ?>
+	
 	<h1 class="entry-title noinfo"><?php the_title(); ?></h1>
 	
 	<div class="content">
