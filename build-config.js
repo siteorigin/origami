3220d5ddbe47ed9e3619035fa2e140113986e5f3
Andrew Misplon <amisplon@gmail.com>
2017-07-17 19:10:18 +0200

    Update build config

--- build-config.js
+++ build-config.js
@@ -55,9 +55,15 @@ module.exports = {
 			'!{tmp,tmp/**}'                     // Ignore tmp/ and contents
 		]
 	},
+    css: {
+        src: [
+            'style.css',
+            'woocommerce.css',
+        ],
+    },	
 	copy: {
 		src: [
-			'**/!(*.js|*.scss|style.css)',                        // Everything except .js and .scss files
+            '**/!(*.js|*.scss|*.md|style.css|woocommerce.css)',   // Everything except .js and .scss files
 			'!{build,build/**}',                                  // Ignore build/ and contents
 			'!{sass,sass/**}',                                    // Ignore sass/ and contents
 			'inc/settings/chosen/*.js',                           // Ensure necessary .js files ignored in the first glob are copied
